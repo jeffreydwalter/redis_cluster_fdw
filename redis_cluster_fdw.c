434328a880eb4c5b4b207f88c391c5ff7093381b
Jeffrey Walter <jeffreydwalter@gmail.com>
2022-04-06 12:35:47 -0500

    Update redis_cluster_fdw.c

--- redis_cluster_fdw.c
+++ redis_cluster_fdw.c
@@ -23,8 +23,9 @@
 #include "postgres.h"
 
 /* check that we are compiling for the right postgres version */
-#if PG_VERSION_NUM < 140000
+#if PG_VERSION_NUM < 140000 || PG_VERSION_NUM  >= 150000
 #error wrong Postgresql version this branch is only for 14.
+#endif
 #endif
 
 
